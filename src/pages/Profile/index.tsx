6cd1123e8befb68b439c19711a2cafc4e75dd8cb
Silvio Cajinca Contreiras <SCContreiras@rd.com.br>
2021-11-10 01:45:51 -0300

    created profile page config

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,8 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import { Header } from './styles'
+import { api } from "../../services/api";
+import logo from '../../assets/logo-github.svg'
+interface RepositoryParams {
+  repository: string;
+}
+
+interface Repository {
+  fullName: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
+
+interface Issue {
+  id: number;
+  title: string;
+  user: {
+    login: string;
+  }
+}
 
 const Profile: React.FC = () => {
+  const [repository, setRepository] = useState<Repository | null>(null);
+
+const { params } = useRouteMatch<RepositoryParams>();
+
+  useEffect(() => {
+    api.get(`repos/${params.repository}`).then(({ data }) => {
+      setRepository(data)
+    })
+  }, []);
+
+
   return (
-    <p>Search</p>
+    <>
+    <Header>
+      <img src={logo} alt="git logo" />
+    </Header>
+      <p>Search</p>
+
+    </>
   )
 };
 
